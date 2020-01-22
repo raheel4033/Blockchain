0f0aeb34077e63787093eb91ebcf18bf89be73cf
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-22 21:49:52 +0500

    Update blockchainds.js
    newBlock gives the arrangement of the new labels to the existing elements and modify the function.

--- blockchainds.js
+++ blockchainds.js
@@ -3,3 +3,21 @@ function Blockchain()
     this.chain = [];
     this.newTransactions=[];
 }
+
+
+Blockchain.prototype.createNewBlockchain = function(nonce,previousBlockHash,hash)
+{
+    const NewBlock
+    {
+        index: this.chain.length+1;
+        timestamp: Date.now();
+        transactions: this.newTransactions;
+        nonce: nonce;
+        hash: hash;
+        previousBlockHash: previousBlockHash
+    };
+    this.newTransactions = [];
+    this.chain.push(NewBlock);
+
+    return NewBlock;
+}
